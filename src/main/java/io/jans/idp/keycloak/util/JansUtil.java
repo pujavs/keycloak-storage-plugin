6a22ac517ee84398a6ad5e8af940962369f2dc71
pujavs <pujas.works@gmail.com>
2023-09-14 16:09:16 +0530

    feat(config-api): scim token generation code
    Signed-off-by: pujavs <pujas.works@gmail.com>

--- src/main/java/io/jans/idp/keycloak/util/JansUtil.java
+++ src/main/java/io/jans/idp/keycloak/util/JansUtil.java
@@ -145,6 +145,139 @@ public class JansUtil {
         Response response = null;
         try {
 
+            TokenRequest tokenRequest = new TokenRequest(GrantType.CLIENT_CREDENTIALS);
+            tokenRequest.setScope(scope);
+            tokenRequest.setAuthUsername(clientId);
+            tokenRequest.setAuthPassword(clientSecret);
+            tokenRequest.setGrantType(GrantType.CLIENT_CREDENTIALS);
+            tokenRequest.setAuthenticationMethod(AuthenticationMethod.CLIENT_SECRET_BASIC);
+            
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("grant_type", GrantType.CLIENT_CREDENTIALS.getValue());
+            parameters.put("username", clientId+":"+clientSecret);
+            parameters.put("password", clientSecret);
+            parameters.put("scope", scope);
+            parameters.put("client_id", clientId);
+            parameters.put("client_secret", clientSecret);
+            parameters.put("authorization_method", AuthenticationMethod.CLIENT_SECRET_BASIC.name());
+            
+            final MultivaluedHashMap<String, String> multivaluedHashMap = new MultivaluedHashMap<>(
+                    parameters);
+           // response = request.post(Entity.form(multivaluedHashMap));
+            
+           HttpClient client = HttpClientBuilder.create().build();
+           LOG.info("\n\n\n\n Final try Krishna @@@@@*** Request for Access Token for Post-  tokenUrl:{}, multivaluedHashMap:{} ", tokenUrl,multivaluedHashMap);
+           
+           try {
+               JsonNode jsonNode1 = SimpleHttp.doPost(tokenUrl, client)
+                       .header("Authorization","Basic " + tokenRequest.getEncodedCredentials())
+                       .header("Content-Type", MediaType.APPLICATION_FORM_URLENCODED)
+                       .param("grant_type", "client_credentials")
+                       .param("username", clientId+":"+clientSecret)
+                       .param("scope", "scope")
+                       .param("client_id", clientId)
+                       .param("client_secret", clientSecret)
+                       .param("authorization_method", "client_secret_basic")
+                       .asJson();
+               LOG.info("\n\n ***** Thankyou Krishna POST Request for Access Token -  jsonNode1:{} ", jsonNode1);
+
+               
+           }catch(Exception ex) {
+               ex.printStackTrace();
+               LOG.error("\n\n\n ********************* Post 1  error is =  "+ex+"*****\n\n\n");
+           }
+               
+                //JsonNode jsonNode2 = SimpleHttp.doPost(tokenUrl, client).header("Authorization","Basic " + tokenRequest.getEncodedCredentials()).header("Content-Type", MediaType.APPLICATION_FORM_URLENCODED).acceptJson().param("user", clientId+":"+clientSecret).json(Entity.form(multivaluedHashMap)).asJson();
+           JsonNode jsonNode2 = SimpleHttp.doPost(tokenUrl, client).header("Authorization","Basic " + tokenRequest.getEncodedCredentials()).header("Content-Type", MediaType.APPLICATION_FORM_URLENCODED).json(Entity.form(multivaluedHashMap)).asJson();
+           LOG.info("POST Request for Access Token -  jsonNode2:{} ", jsonNode2);
+
+        } finally {
+
+            if (response != null) {
+                response.close();
+            }
+        }
+        return null;
+    }
+    
+    /*--------------------------------------------*/
+    public static TokenResponse requestAccessToken_TokenRequest(final String tokenUrl, final String clientId,
+            final String clientSecret, final String scope) {
+        LOG.info(" Latest Request for Access Token -  tokenUrl:{}, clientId:{}, clientSecret:{}, scope:{} ", tokenUrl,
+                clientId, clientSecret, scope);
+        Response response = null;
+        try {
+            TokenRequest tokenRequest = new TokenRequest(GrantType.CLIENT_CREDENTIALS);
+            tokenRequest.setScope(scope);
+            tokenRequest.setAuthUsername(clientId);
+            tokenRequest.setAuthPassword(clientSecret);
+            Builder request = getClientBuilder(tokenUrl);
+            request.header("Authorization", "Basic " + tokenRequest.getEncodedCredentials());
+            request.header("Content-Type", MediaType.APPLICATION_FORM_URLENCODED);
+            final MultivaluedHashMap<String, String> multivaluedHashMap = new MultivaluedHashMap<>(
+                    tokenRequest.getParameters());
+            response = request.post(Entity.form(multivaluedHashMap));
+            LOG.info("Response for Access Token -  response:{}", response);
+            if (response.getStatus() == 200) {
+                String entity = response.readEntity(String.class);
+                TokenResponse tokenResponse = new TokenResponse();
+                tokenResponse.setEntity(entity);
+                tokenResponse.injectDataFromJson(entity);
+                return tokenResponse;
+            }
+        } finally {
+
+            if (response != null) {
+                response.close();
+            }
+        }
+        return null;
+    }
+    
+    private static TokenResponse requestAccessToken8(final String tokenUrl, final String clientId,
+            final String clientSecret, final String scope) throws IOException{
+        LOG.error("Request for Access Token -  tokenUrl:{}, clientId:{}, clientSecret:{}, scope:{} ", tokenUrl,
+                clientId, clientSecret, scope);
+        Response response = null;
+        try {
+
+            TokenRequest tokenRequest = new TokenRequest(GrantType.CLIENT_CREDENTIALS);
+            tokenRequest.setScope(scope);
+            tokenRequest.setAuthUsername(clientId);
+            tokenRequest.setAuthPassword(clientSecret);
+            tokenRequest.setGrantType(GrantType.CLIENT_CREDENTIALS);
+            tokenRequest.setAuthenticationMethod(AuthenticationMethod.CLIENT_SECRET_BASIC);
+//            Builder request = getClientBuilder(tokenUrl);
+//            request.header("Authorization", "Basic " + tokenRequest.getEncodedCredentials());
+//            request.header("Content-Type", MediaType.APPLICATION_FORM_URLENCODED);
+            final MultivaluedHashMap<String, String> multivaluedHashMap = new MultivaluedHashMap<>(
+                    tokenRequest.getParameters());
+           // response = request.post(Entity.form(multivaluedHashMap));
+            
+           HttpClient client = HttpClientBuilder.create().build();
+           LOG.info("\n\n\n\n @@@@@*** Request for Access Token for Post-  tokenUrl:{}, multivaluedHashMap:{} ", tokenUrl,multivaluedHashMap);
+           
+               
+                JsonNode jsonNode2 = SimpleHttp.doPost(tokenUrl, client).header("Authorization","Basic " + tokenRequest.getEncodedCredentials()).header("Content-Type", MediaType.APPLICATION_FORM_URLENCODED).acceptJson().param("user", clientId+":"+clientSecret).json(Entity.form(multivaluedHashMap)).asJson();
+           
+            LOG.info("POST Request for Access Token -  jsonNode2:{} ", jsonNode2);
+
+        } finally {
+
+            if (response != null) {
+                response.close();
+            }
+        }
+        return null;
+    }
+    
+    private static TokenResponse requestAccessToken7(final String tokenUrl, final String clientId,
+            final String clientSecret, final String scope) throws IOException{
+        LOG.error("Request for Access Token -  tokenUrl:{}, clientId:{}, clientSecret:{}, scope:{} ", tokenUrl,
+                clientId, clientSecret, scope);
+        Response response = null;
+        try {
+
            Map<String, String> parameters = new HashMap<>();
            parameters.put("grant_type", GrantType.CLIENT_CREDENTIALS.getValue());
            parameters.put("username", clientId+":"+clientSecret);
@@ -159,14 +292,14 @@ public class JansUtil {
            HttpClient client = HttpClientBuilder.create().build();
                 LOG.info("\n\n\n\n @@@@@*** Request for Access Token for Post-  tokenUrl:{}, parameters:{} ", tokenUrl,parameters);
             //JsonNode jsonNode = SimpleHttp.doGet(tokenUrl, client).authBasic(clientId, clientSecret).json(parameters).asJson();
-                try {
+          /*      try {
             JsonNode jsonNode = SimpleHttp.doGet(tokenUrl, client).acceptJson().param("user", clientId+":"+clientSecret).json(parameters).asJson();
             LOG.info("\n\n GET Request for Access Token -  jsonNode:{} ", jsonNode);
                 }catch(Exception ex) {
                     LOG.error("\n\n GET Request for Access Token ex:{} ", ex);
                 }
-            
-                JsonNode jsonNode2 = SimpleHttp.doPost(tokenUrl, client).acceptJson().param("user", clientId+":"+clientSecret).json(parameters).asJson();
+            */
+                JsonNode jsonNode2 = SimpleHttp.doPost(tokenUrl, client).header(clientSecret, scope).acceptJson().param("user", clientId+":"+clientSecret).json(parameters).asJson();
             //JsonNode jsonNode = SimpleHttp.doGet(tokenUrl, client).json(parameters).asJson();
             LOG.info("POST Request for Access Token -  jsonNode2:{} ", jsonNode2);
 
