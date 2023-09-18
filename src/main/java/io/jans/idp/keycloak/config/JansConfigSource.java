07d32a8d9b9f323df2eb03086faa50cb0f03ad66
pujavs <pujas.works@gmail.com>
2023-09-18 23:14:18 +0530

    feat(config-api): added validations and config wiring
    Signed-off-by: pujavs <pujas.works@gmail.com>

--- src/main/java/io/jans/idp/keycloak/config/JansConfigSource.java
+++ src/main/java/io/jans/idp/keycloak/config/JansConfigSource.java
@@ -2,6 +2,7 @@ package io.jans.idp.keycloak.config;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.util.Collections;
 import java.util.HashMap;
@@ -26,9 +27,9 @@ public class JansConfigSource implements ConfigSource {
     private static final String CONFIG_FILE_NAME = "jans-keycloak-storage-api.properties";
     private Properties properties = null;
     Map<String, String> propertiesMap = new HashMap<>();
-    
+
     public JansConfigSource() {
-        this.CONFIG_FILE_PATH =  System.getProperty(Constants.JANS_CONFIG_PROP_PATH);
+        this.CONFIG_FILE_PATH = System.getProperty(Constants.JANS_CONFIG_PROP_PATH);
         LOG.info("\n\n this.CONFIG_FILE_PATH:{}", CONFIG_FILE_PATH);
     }
 
@@ -110,7 +111,7 @@ public class JansConfigSource implements ConfigSource {
 
     private Properties loadProperties() {
         LOG.info("\n\n\n ***** JansConfigSource::loadProperties() - Properties form Config.Scope ");
-
+        FileInputStream file = null;
         try {
             // Get file path
             String filePath = getQualifiedFileName();
@@ -119,29 +120,27 @@ public class JansConfigSource implements ConfigSource {
             if (StringUtils.isNotBlank(filePath)) {
 
                 // load the file handle for main.properties
-                FileInputStream file = new FileInputStream(filePath);
+                file = new FileInputStream(filePath);
                 LOG.info("\n\n\n ***** JansConfigSource::loadProperties() - file =" + file + "\n\n");
                 ClassLoader loader = Thread.currentThread().getContextClassLoader();
 
-                try (InputStream inputStream = loader.getResourceAsStream(getQualifiedFileName())) {
+                if (file != null) {
+                    LOG.info("\n\n\n ***** RemoteUserStorageProviderFactory::readFile() - loading file \n\n");
+                    // load all the properties from this file
                     properties = new Properties();
-                    properties.load(inputStream);
+                    properties.load(file);
                     properties.stringPropertyNames().stream()
                             .forEach(key -> propertiesMap.put(key, properties.getProperty(key)));
                     LOG.debug("\n\n JansConfigSource()::loadProperties() - properties :{} {}", properties, "\n\n\n");
-                   
-                } catch (Exception e) {
-                    LOG.error("\n\n Failed to load configuration from : " + filePath, e);
-                    throw new WebApplicationException("Failed to load configuration from " + filePath, e);
+                    if (properties != null) {
+                        printProperties(properties);
+                    }
+                    // we have loaded the properties, so close the file handle
+
+                } else {
+                    LOG.error("Config properties file is null!");
+                    throw new ComponentValidationException("CConfig properties file is null!!!");
                 }
-                LOG.info("\n\n\n ***** JansConfigSource::loadProperties() - loading file \n\n");
-                // load all the properties from this file
-                properties.load(file);
-                properties = new Properties();
-                properties.load(file);
-                properties.stringPropertyNames().stream()
-                        .forEach(key -> propertiesMap.put(key, properties.getProperty(key)));
-                LOG.info("\n\n JansConfigSource()::loadProperties() - properties :{} {}", properties, "\n\n\n");
 
                 if (properties != null) {
                     printProperties(properties);
@@ -149,8 +148,7 @@ public class JansConfigSource implements ConfigSource {
                     LOG.error("Could not load config properties!");
                     throw new ComponentValidationException("Could not load config properties!!!");
                 }
-                // we have loaded the properties, so close the file handle
-                file.close();
+
             } else {
                 LOG.error("Property file is null!");
                 throw new ComponentValidationException("Config property file is null!!!");
@@ -159,6 +157,13 @@ public class JansConfigSource implements ConfigSource {
         } catch (Exception ex) {
             LOG.error("Failed to load property file", ex);
             throw new ComponentValidationException("Failed to load property file!!!");
+        } finally {
+            try {
+                if (file != null) {
+                    file.close();
+                }
+            } catch (IOException ex) {
+            }
         }
         return properties;
     }
